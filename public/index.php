bc802852e1044351d7c27cf4370ce852b8e5c7a0
chancellor <pavel@zolotarev.pp.ua>
2018-11-20 22:01:08 +0200

    second home task

--- public/index.php
+++ public/index.php
@@ -35,8 +35,15 @@ fclose($fp); // закрывает поток
 $arr = json_decode($mytext, true); // преобразование json строки в массив
 
 foreach ($arr as $key => $row) {
-    $arr[$key]['category'] = $category[$row['category']]; // замена номеров категорий на их имена
+    $arr[$key]['categoryName'] = $category[$row['category']]; // замена номеров категорий на их имена
 }
+$categorySelector = '<form method="get"><select name="search">';
+foreach ($category as $key => $cat) {
+    $condition = $_REQUEST['search'] == $key ? 'selected' : '';
+    $categorySelector .= '<option value="' . $key . '" ' . $condition . '> ' . $cat . '</option>';
+}
+$categorySelector .= '</select><input type="submit" value="Send"></form>';
+echo $categorySelector;
 
 # сортировка массива на основе параметров запроса
 switch (str_replace('-', '', $_REQUEST['sort'])) {
@@ -73,4 +80,92 @@ $sortOrder = 'arrow' . str_replace(' ', '', ucwords(str_replace('_', ' ', str_re
 $$sortOrder = $arrow; // присвоение текущей стрелки
 usort($arr, 'mnsort'); // применение сортировки инициализированной в switch функции
 
-include ('template.html'); // подключение представления
+//include('template.html'); // подключение представления
+
+# формирование переменной таблицы
+$table = '
+<style>
+    select, input{
+        height: 30px;
+        width: 200px;
+        font-size: 20px;
+    }
+    .container {
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+    }
+    table, tr, th, td {
+        text-align: center;
+        border: 1px solid black;
+        border-collapse: collapse;
+        padding: 5px;
+    }
+    table {
+        width: 100%;
+    }
+    thead th {
+        line-height: 25px;
+        position: sticky;
+        top: -2px;
+    }
+    .leftText{
+        text-align: left;
+    }
+    .leftRight{
+        text-align: right;
+    }
+    tr:hover{
+        background: aqua;
+    }
+    th{
+        text-transform: uppercase;
+        background: #ededed;
+        color: #555;
+    }
+    th a{
+        color: #000fbc;
+        text-decoration: none;
+        padding: 5px 10px;
+    }
+    th span{
+        color: #555;
+        font-size: 1.1em;
+    }
+    p,center {
+        margin: 0 auto;
+        width: 200px;
+    }
+</style>
+<div class="container">
+    <h1>Home Task</h1>
+    <table><thead><tr style="position: sticky;top: 0px;">
+        <th><span>' . @$arrowId . '</span><a href="?sort=' . $descParam . 'id">id</a><span>' . @$arrowId . '</span></th>
+        <th>user link</th>
+        <th>comment</th>
+        <th><span>' . @$arrowCategory . '</span><a href="?sort=' . $descParam . 'category">category</a><span>' . @$arrowCategory . '</span></th>
+        <th><span>' . @$arrowTotalSpent . '</span><a href="?sort=' . $descParam . 'total_spent">total spent, usd</a><span>' . @$arrowTotalSpent . '</span></th>
+        <th><span>' . @$arrowCreatedAt . '</span><a href="?sort=' . $descParam . 'created_at">created at</a><span>' . @$arrowCreatedAt . '</span></th>
+    </tr></thead>';
+foreach ($arr as $row) {
+    if ($_REQUEST['search'] != $row['category']) continue;
+    # форматирование значений перед записью в переменную таблицы
+    $table .= '<tr>
+            <td class="leftRight">' . $row['id'] . '</td>
+            <td>' . $row['user_link'] . '</td>
+            <td class="leftText" title="' . $row['comment'] . '">' . mb_strimwidth($row['comment'], 0, 100, "...") . '</td>
+            <td>' . $row['categoryName'] . '</td>
+            <td>$ ' . $row['total_spent'] . '</td>
+            <td>' . date('Y-m-d', $row['created_at']) . '</td>
+        </tr></div>';
+}
+
+$table .= '<tr>
+    <th><span>' . @$arrowId . '</span><a href="?sort=' . $descParam . 'id">id</a><span>' . @$arrowId . '</span></th>
+    <th>user link</th>
+    <th>comment</th>
+    <th><span>' . @$arrowCategory . '</span><a href="?sort=' . $descParam . 'category">category</a><span>' . @$arrowCategory . '</span></th>
+    <th><span>' . @$arrowTotalSpent . '</span><a href="?sort=' . $descParam . 'total_spent">total spent</a><span>' . @$arrowTotalSpent . '</span></th>
+    <th><span>' . @$arrowCreatedAt . '</span><a href="?sort=' . $descParam . 'created_at">created at</a><span>' . @$arrowCreatedAt . '</span></th>
+</tr></table>';
+echo $table, '<p class="center">Array size: ' . sizeof($arr) . ' elements</p>'; // вывод таблицы
