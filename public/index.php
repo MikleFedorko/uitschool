160a1ec2f983082677022334f986ac2e61575134
chancellor <pavel@zolotarev.pp.ua>
2018-11-24 14:50:31 +0200

    third home task

--- public/index.php
+++ public/index.php
@@ -10,45 +10,73 @@
  *
  *
  ************************************************************************/
-
 $descParam = '-'; // начальное значение параметра типа сортировки для формирования ссылки
 $arrow = '&uarr;'; // начальное значение типа стрелки
-
 # Json со списом категорий
 $categoryJson = '{"4":"Assets","7":"Christmas","2":"Clothes","3":"Easter","5":"Gameplay","8":"Halloween","6":"Release theme","1":"Scenery","10":"St. Patrick\'s","9":"St.Valentine","11":"Stylist"}';
 $category = json_decode($categoryJson, true);// преобразование json строки в массив
-
 $descStatus = false; // начальное значение для переменной хранящей состояние обратной сортировки для проверки условий
-if ($_REQUEST['sort'][0] == '-') { // проверка параметра пришедшего из ссылки. если условие выполняется меняем значения по умолчанию
+
+if($_REQUEST['sort'][0] == '-') { // проверка параметра пришедшего из ссылки. если условие выполняется меняем значения по умолчанию
     $descStatus = true;
     $descParam = '';
     $arrow = '&darr;';
 }
 
-$fp = fopen('testJsonDataToSort.txt', 'r'); // закрепляет именованый ресурс, указанный в аргументе filename, за потоком.
+$filename = 'testJsonDataToSort.txt'; // закрепляет именованый ресурс, указанный в аргументе filename, за потоком.
+if (!$fp = fopen($filename, 'r')) {
+    echo "Не могу открыть файл ($filename)";
+    exit;
+}
+
 $mytext = ''; // пустая переменная для записи данных из файла
 while (!feof($fp)) { // проверка что указатель файла не достиг End Of File (EOF)
     $mytext .= fgets($fp); // функция берет чанк символо узаканной длинный из файла
 }
 fclose($fp); // закрывает поток
-
 $arr = json_decode($mytext, true); // преобразование json строки в массив
-
-foreach ($arr as $key => $row) {
+foreach($arr as $key => $row) {
     $arr[$key]['categoryName'] = $category[$row['category']]; // замена номеров категорий на их имена
 }
-$categorySelector = '<form method="get"><select name="search">';
-foreach ($category as $key => $cat) {
+
+if (isset($_REQUEST['item']) && isset($_REQUEST['update_cat'])) {
+    $arr[$_REQUEST['item']]['category'] = $_REQUEST['update_cat'];
+//    var_dump(json_encode($arr));die;
+    if (!$handle = fopen($filename, 'w')) {
+        echo "Не могу открыть файл ($filename)";
+        exit;
+    }
+    if (fwrite($handle, json_encode($arr)) === FALSE) {
+        echo "Не могу произвести запись в файл ($filename)";
+        exit;
+    }
+    fclose($handle);
+}
+
+$categorySelector = '<form method="get"><select name="search" class="filter">';
+foreach($category as $key => $cat) {
     $condition = $_REQUEST['search'] == $key ? 'selected' : '';
     $categorySelector .= '<option value="' . $key . '" ' . $condition . '> ' . $cat . '</option>';
 }
 $categorySelector .= '</select><input type="submit" value="Send"></form>';
-echo $categorySelector;
+
+function showCategoriesSelector($categoryList, $categoryNumber, $item)
+{
+    $categorySelector = '<form method="get">
+    <input type="hidden" value="' . $item . '" name="item" />
+    <select name="update_cat" onchange="this.form.submit();">';
+    foreach($categoryList as $key => $cat) {
+        $condition = $categoryNumber == $key ? 'selected' : '';
+        $categorySelector .= '<option value="' . $key . '" ' . $condition . '> ' . $cat . '</option>';
+    }
+    $categorySelector .= '</select></form>';
+    return $categorySelector;
+}
 
 # сортировка массива на основе параметров запроса
 switch (str_replace('-', '', $_REQUEST['sort'])) {
     case 'category':
-        if ($descStatus) {
+        if($descStatus) {
             function mnsort($a, $b)
             {
                 return strnatcmp($b['category'], $a['category']); // применение человекоподобного алгоритма для сортировки строк
@@ -61,7 +89,7 @@ switch (str_replace('-', '', $_REQUEST['sort'])) {
         }
         break;
     default:
-        if ($descStatus) {
+        if($descStatus) {
             function mnsort($a, $b)
             {
                 $sortName = str_replace('-', '', $_REQUEST['sort']); // удаление лишнего символа для получения текущего поля сортировки
@@ -75,17 +103,14 @@ switch (str_replace('-', '', $_REQUEST['sort'])) {
         }
         break;
 }
-
 $sortOrder = 'arrow' . str_replace(' ', '', ucwords(str_replace('_', ' ', str_replace('-', '', $_REQUEST['sort'])))); // формирование имени переменной хранящей текущую стрелку
 $$sortOrder = $arrow; // присвоение текущей стрелки
 usort($arr, 'mnsort'); // применение сортировки инициализированной в switch функции
-
 //include('template.html'); // подключение представления
-
 # формирование переменной таблицы
 $table = '
 <style>
-    select, input{
+    select.filter, input{
         height: 30px;
         width: 200px;
         font-size: 20px;
@@ -139,32 +164,33 @@ $table = '
 </style>
 <div class="container">
     <h1>Home Task</h1>
+    ' . $categorySelector . '
     <table><thead><tr style="position: sticky;top: 0px;">
         <th><span>' . @$arrowId . '</span><a href="?sort=' . $descParam . 'id">id</a><span>' . @$arrowId . '</span></th>
         <th>user link</th>
         <th>comment</th>
-        <th><span>' . @$arrowCategory . '</span><a href="?sort=' . $descParam . 'category">category</a><span>' . @$arrowCategory . '</span></th>
+        <th><span>' . @$arrowCategory . '</span><a href="?sort=' . $descParam . 'categoryName">category</a><span>' . @$arrowCategory . '</span></th>
         <th><span>' . @$arrowTotalSpent . '</span><a href="?sort=' . $descParam . 'total_spent">total spent, usd</a><span>' . @$arrowTotalSpent . '</span></th>
         <th><span>' . @$arrowCreatedAt . '</span><a href="?sort=' . $descParam . 'created_at">created at</a><span>' . @$arrowCreatedAt . '</span></th>
     </tr></thead>';
-foreach ($arr as $row) {
-    if ($_REQUEST['search'] != $row['category']) continue;
+foreach($arr as $row) {
+    if($_REQUEST['search'] && $_REQUEST['search'] != $row['category']) continue;
+    $categorySelector = showCategoriesSelector($category, $row['category'], $row['id']);
     # форматирование значений перед записью в переменную таблицы
     $table .= '<tr>
-            <td class="leftRight">' . $row['id'] . '</td>
-            <td>' . $row['user_link'] . '</td>
-            <td class="leftText" title="' . $row['comment'] . '">' . mb_strimwidth($row['comment'], 0, 100, "...") . '</td>
-            <td>' . $row['categoryName'] . '</td>
-            <td>$ ' . $row['total_spent'] . '</td>
-            <td>' . date('Y-m-d', $row['created_at']) . '</td>
+            <td class="leftRight" title="' . strlen($row['id']) . ' symbols">' . $row['id'] . '</td>
+            <td title="' . strlen($row['user_link']) . ' symbols">' . $row['user_link'] . '</td>
+            <td class="leftText" title="' . strlen($row['comment']) . ' symbols">' . mb_strimwidth($row['comment'], 0, 100, "...") . '</td>
+            <td title="' . strlen($row['categoryName']) . ' symbols">' . $categorySelector . '</td>
+            <td title="' . (strlen($row['total_spent']) + 1) . ' symbols">$' . $row['total_spent'] . '</td>
+            <td title="' . strlen(date('Y-m-d', $row['created_at'])) . ' symbols">' . date('Y-m-d', $row['created_at']) . '</td>
         </tr></div>';
 }
-
 $table .= '<tr>
     <th><span>' . @$arrowId . '</span><a href="?sort=' . $descParam . 'id">id</a><span>' . @$arrowId . '</span></th>
     <th>user link</th>
     <th>comment</th>
-    <th><span>' . @$arrowCategory . '</span><a href="?sort=' . $descParam . 'category">category</a><span>' . @$arrowCategory . '</span></th>
+    <th><span>' . @$arrowCategory . '</span><a href="?sort=' . $descParam . 'categoryName">category</a><span>' . @$arrowCategory . '</span></th>
     <th><span>' . @$arrowTotalSpent . '</span><a href="?sort=' . $descParam . 'total_spent">total spent</a><span>' . @$arrowTotalSpent . '</span></th>
     <th><span>' . @$arrowCreatedAt . '</span><a href="?sort=' . $descParam . 'created_at">created at</a><span>' . @$arrowCreatedAt . '</span></th>
 </tr></table>';
