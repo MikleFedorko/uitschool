6a8f3bd1de143f0c7228786f3a92524fa9e622e4
acranum1 <Zoloac88H>
2018-11-27 15:44:21 +0200

    add search&sort cookies

--- modules/json_table.inc.php
+++ modules/json_table.inc.php
@@ -32,17 +32,17 @@ $category = json_decode($categoryJson, true);// преобразование jso
 $descStatus = false; // начальное значение для переменной хранящей состояние обратной сортировки для проверки условий
 
 # Сохраняю в куки состояние фильтра и сортировки
-if (isset($_REQUEST['search'])) setcookie("search", $_REQUEST['search']);
-if (!isset($_REQUEST['search'])
+if(isset($_REQUEST['search'])) setcookie("search", $_REQUEST['search']);
+if(!isset($_REQUEST['search'])
     && !empty($_COOKIE['search']))
     $_REQUEST['search'] = $_COOKIE['search'];
 
-if (isset($_REQUEST['sort'])) setcookie("sort", $_REQUEST['sort']);
-if (!isset($_REQUEST['sort'])
+if(isset($_REQUEST['sort'])) setcookie("sort", $_REQUEST['sort']);
+if(!isset($_REQUEST['sort'])
     && !empty($_COOKIE['sort']))
     $_REQUEST['sort'] = $_COOKIE['sort'];
 
-if (isset($_REQUEST['sort']) && $_REQUEST['sort'][0] == '-') { // проверка параметра пришедшего из ссылки. если условие выполняется меняем значения по умолчанию
+if(isset($_REQUEST['sort']) && $_REQUEST['sort'][0] == '-') { // проверка параметра пришедшего из ссылки. если условие выполняется меняем значения по умолчанию
     $descStatus = true;
     $descParam = '';
     $arrow = '&darr;';
@@ -51,17 +51,17 @@ if (isset($_REQUEST['sort']) && $_REQUEST['sort'][0] == '-') { // проверк
 $mytext = freader($filename);
 $arr = json_decode($mytext, true); // преобразование json строки в массив
 
-foreach ($arr as $key => $row) {
+foreach($arr as $key => $row) {
     $arr[$key]['categoryName'] = $category[$row['category']]; // замена номеров категорий на их имена
 }
 
-if (isset($_REQUEST['item']) && isset($_REQUEST['update_cat'])) {
+if(isset($_REQUEST['item']) && isset($_REQUEST['update_cat'])) {
     $arr[$_REQUEST['item']]['category'] = $_REQUEST['update_cat'];
     fwriter($filename, $arr);
 }
 
 $arr = sorter($arr, $descStatus);
-if (isset($_REQUEST['sort'])) {
+if(isset($_REQUEST['sort'])) {
     $sortOrder = 'arrow' . str_replace(' ', '', ucwords(str_replace('_', ' ', str_replace('-', '', $_REQUEST['sort'])))); // формирование имени переменной хранящей текущую стрелку
     $$sortOrder = $arrow; // присвоение текущей стрелки
 }
